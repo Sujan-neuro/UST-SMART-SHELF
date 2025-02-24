2633e16d7c446bf96152ce1083379ba9b650cc07
Sujan Das <sujan.das@neurologicai.com>
2025-02-24 22:14:42 +0530

    Modified config file

--- src/config.py
+++ src/config.py
@@ -1,21 +1,8 @@
-ID2LABEL = {
-    0: "0-2",
-    1: "3-9",
-    2: "10-19",
-    3: "20-29",
-    4: "30-39",
-    5: "40-49",
-    6: "50-59",
-    7: "60-69",
-    8: "more than 70",
-}
-
-AGE_DETECTOR = "nateraw/vit-age-classifier"
+AGE_DETECTOR_WEIGHTS = "./src/resnet50_regression.pth"
 GENDER_DETECTOR = "rizvandwiki/gender-classification"
-PEOPLE_DETECTOR = "yolov8m.pt"
 CONFIDENCE_THRESHOLD = 0.8
 EMBEDDING_THRESHOLD = 0.18
 # TRACKING_DURATION_SEC should be more than PROCESS_INTERVAL_SEC always
-PROCESS_INTERVAL_SEC = 2
+PROCESS_INTERVAL_SEC = 1
 TRACKING_DURATION_SEC = 3
 MAX_EMBEDDING_TO_MATCH = 500
