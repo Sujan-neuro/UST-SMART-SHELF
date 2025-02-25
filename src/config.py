a4258ee41a6bf90ff5c314cbf2558217c4b93a6a
Sujan Das <sujan.das@neurologicai.com>
2025-02-25 16:56:29 +0530

    Added STORE LOCATION

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from utils import get_current_location
+
 AGE_DETECTOR_WEIGHTS = "./src/resnet50_regression.pth"
 GENDER_DETECTOR = "rizvandwiki/gender-classification"
 CONFIDENCE_THRESHOLD = 0.8
@@ -6,3 +8,7 @@ EMBEDDING_THRESHOLD = 0.18
 PROCESS_INTERVAL_SEC = 1
 TRACKING_DURATION_SEC = 3
 MAX_EMBEDDING_TO_MATCH = 500
+LOCATION_ID = "6643631e2fd6df5a3e669233"
+
+STORE_LOCATION = get_current_location()
+TOPICS = ['nrf_singapore']
