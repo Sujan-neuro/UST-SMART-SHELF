97cac35958111fe340ef8a2d32505d37c518ddbd
Samiran13 <samiran.dolui@neurologicai.com>
2025-11-06 16:03:10 +0530

    random event sent via api in case of no detection

--- src/configs/config.py
+++ src/configs/config.py
@@ -20,11 +20,12 @@ MAX_EMBEDDING_TO_MATCH = 500
 LOCATION_ID = "..."
 
 # API CONFIGURATION
-BASE_URL = "https://demo.personalisationhub.com"  # Change this line
-API_KEY = "dISlwI3whWXjgKY31Naz2Z7qkIwpdPia5pyHVtBzPg04ZijOkMCM3nnc35voCBqYXPPhjEOpr0asmIYWsiFWyIFMp0SRAj0DjtG8Jfj1qjhodOFKIIX6b0yRZZzEtDF1"        # Change this line
+BASE_URL = "https://pfrm.fcust.com"  # Change this line
+API_KEY = "AGGJ4FpR0lJHv9Vjedc49K7A2Nrqv9OVXnVt6TWYoNUWRdquPY2pHfg39BxoObP5utcfL4mvNqGkwAt7X7ochdGitpB001TixxfRGHp181jxiFOnu4ufwUCMrUlZ2ce9"        # Change this line
+
 # API PAYLOAD CONFIGURATION
 DEFAULT_DEVICE_TYPE = ""
-DEFAULT_EVENTS = ["sales"]
+DEFAULT_EVENTS = ["white","black"]
 DEFAULT_COMPANY_NAME = "UST"
 
 # Configurable parameters (can be modified as needed)
@@ -32,7 +33,7 @@ PURCHASE_INTENT = "electronics"  # Purchase intent from config
 VISITOR_SEGMENTS = ["female"]  # Visitor segments from config
 
 # Display targeting options (choose one approach)
-DISPLAY_IDS = [923]  # Display IDs from config - for single/multiple display targeting
+DISPLAY_IDS = [746]  # Display IDs from config - for single/multiple display targeting
 STORE_UUID = "UST-001"    # For all store displays targeting
 STORE_UUID_TYPE = "STORE_CODE"
 
